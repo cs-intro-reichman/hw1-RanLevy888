3616302d36d397cc742482a7b6330f4356d17e77
Ran Levy <Ran.levy02@post.runi.ac.il>
2025-10-31 11:54:02 +0200

    add the done

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,10 @@
 // Generates three integer random numbers in a given range,
+
+import java.util.Random;
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
-	}
+		int Number = Integer.parseInt(args[0]);
+	    
+ 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		double n = Double.parseDouble(args[2]);
+      double futureValue = currentValue*Math.pow(1+rate,n);
+	  System.out.println("After "+n+" years, " +currentValue +
+	  " saved at "+ rate+"% will yield "+futureValue);
 	}
 }
