93350303e9380b5529e7a0a267ce01dd0d24bbec
Ran Levy <Ran.levy02@post.runi.ac.il>
2025-11-06 17:43:06 +0200

    fixx

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,28 +3,17 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-	
-        
-		if(minutes<10)
-		{
-			if(hours==12){
-             System.out.println(hours-12 + ":0" +minutes + " PM");
-			}
-		if(hours<12){
-		System.out.println(hours + ":0" + minutes +" AM");
-	}else{
-		System.out.println(hours-12 + ":0" +minutes + " PM");
-	}
-}else{
-	if(hours==12){
-             System.out.println(hours-12 + ":0" +minutes + " PM");
-			}
-if(hours<12){
-		System.out.println(hours + ":" + minutes +" AM");
-	}else{
-		System.out.println(hours-12 + ":" +minutes + " PM");
-	}
-}
+		String min = Integer.toString(minutes);
+		if (minutes < 10) {
+			min = "0" + minutes;
+		}
+		if (hours >= 12) {
+			if(hours > 12)
+			hours = hours - 12;
+			System.out.println(hours + ":" + min + " PM");
+		} else {
+			System.out.println(hours + ":" + min + " AM");
+		}
 
 }
 }
